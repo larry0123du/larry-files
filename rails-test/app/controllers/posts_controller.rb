33f011f16e4453fe5938ab0597d519d8290130bf
larry0123du <drh6693@sina.cn>
2015-04-25 11:42:27 +0800

    hws

---
+++ rails-test/app/controllers/posts_controller.rb
@@ -0,0 +1,82 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :reply]
+  before_action :authenticate_user!
+
+  # GET /posts
+  def index
+    @posts = Post.all.page(params[:page])
+    respond_to do |format|
+      format.html { render }
+      format.json do
+        render json: @posts
+      end
+    end
+    
+  end
+
+  def hottest
+    @posts = Post.hottest
+    render :index
+  end
+
+  # GET /posts/1
+  def show
+    @post.clicks += 1
+    @post.save
+  end
+
+  def reply
+    @post.replies.create(params[:reply].permit(:body))
+    @post.replies.user = current_user
+    redirect_to :back, notice: "Reply created successfully"
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    
+    # @post = current_user.posts.new(params[:post])
+    @post.user = current_user
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
